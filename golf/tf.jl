1a2c0bdd85e5279154ea2c5e81756c3152a63f2a
m.campbell <m.campbell@tutanota.com>
2023-09-21 17:35:52 +0100

    a first pass at golf

---
+++ golf/tf.jl
@@ -0,0 +1,14 @@
+#! /usr/bin/env julia
+
+# golf/tf.jl
+
+using StatsBase
+
+stop_words = split(read(open("../stop_words.txt"), String), ",")
+words = [w.match for w in eachmatch(r"[a-z]{2,}", read(open(ARGS[1]), String) |> lowercase)]
+counts = StatsBase.countmap([w for w in words if !(w in stop_words)])
+for (k, v) in sort(collect(counts); by = x -> x.second, rev = true)[1:25]
+    println(k, " - ", v)
+end
+
+### end
