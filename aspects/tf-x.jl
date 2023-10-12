472dc0d612caed7bd3166b596e53cd77ea490b0a
m.campbell <m.campbell@tutanota.com>
2023-10-12 17:17:57 +0100

    tidy up usage and handling of julia load path

--- aspects/tf-x.jl
+++ aspects/tf-x.jl
@@ -1,9 +1,13 @@
 #! /usr/bin/env julia
 
+##
+## usage: define the environment variable JULIA_LOAD_PATH and run the script `tf-x.jl`, e.g.
+##
+##  JULIA_LOAD_PATH="$JULIA_LOAD_PATH:$(pwd)" ./tf-x.jl ../pride-and-prejudice.txt
+##
 
-push!(LOAD_PATH, pwd())
 
-using A     # or using TF to run with profiling
+using A     # or using TF to run without profiling
 
 
 frequencies = order(counts(extract_words(ARGS[1])))
